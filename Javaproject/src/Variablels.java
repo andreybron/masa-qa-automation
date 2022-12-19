8af5bb52da3cb02e9e57341252f94432b6171bec
Andrey <46926928+andreybron@users.noreply.github.com>
2022-12-19 20:51:33 +0200

    Create Variables

---
+++ Javaproject/src/Variablels.java
@@ -0,0 +1,22 @@
+public class Variablels {
+    public static void main(String[] args) {
+        byte myByte;
+
+        myByte = 4;
+
+        byte myByte2 = -127;
+
+        short myShort = 2345;
+
+        int myInt = 123456789;
+
+        long myLong = 123242424243242423L;
+
+        System.out.println("myByte=" + myByte);
+        System.out.println(myByte2);
+        System.out.println("myByte2 = " + myByte2);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+    }
+}
