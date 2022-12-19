d24b156540fa3889701d19971c0aa72215859b1d
Andrey <46926928+andreybron@users.noreply.github.com>
2022-12-19 21:23:27 +0200

    Update Variables files

--- Javaproject/src/Variablels.java
+++ Javaproject/src/Variablels.java
@@ -12,11 +12,30 @@ public class Variablels {
 
         long myLong = 123242424243242423L;
 
+        float myFloat = 4.69f;
+
+        double myDouble =99.99;
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char myChar = '\u2607';
+        char myChar2 = 'B';
+
+        String myName = "Andrey";
+
         System.out.println("myByte=" + myByte);
         System.out.println(myByte2);
         System.out.println("myByte2 = " + myByte2);
         System.out.println(myShort);
         System.out.println(myInt);
         System.out.println(myLong);
+        System.out.println("myFloat = " + myFloat); // myFloat.soutv
+        System.out.println("myDouble = " + myDouble);
+        System.out.println("isTrue = " + isTrue);
+        System.out.println("isFalse = " + isFalse);
+        System.out.println("myChar = " + myChar);
+        System.out.println("myChar2 = " + myChar2);
+        System.out.println("myName = " + myName);
     }
 }
