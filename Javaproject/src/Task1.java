4055be9d53b93aeb6504ee89d3ee0b195a3a009a
Andrey <46926928+andreybron@users.noreply.github.com>
2022-12-19 19:26:35 +0200

    Lesson 3

---
+++ Javaproject/src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.print("\t\tРУБАИ\n");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("\t\t\t\tО. Хайям");
+    }
+}
