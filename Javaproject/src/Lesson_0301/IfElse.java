b42da12d4f5e86f6326b366a90b885085242cd24
Andrey <46926928+andreybron@users.noreply.github.com>
2022-12-26 19:15:21 +0200

    IfElse

---
+++ Javaproject/src/Lesson_0301/IfElse.java
@@ -0,0 +1,19 @@
+package Lesson_0301;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter age..");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("U're adult");
+        }
+        else {
+            System.out.println("U're young");
+        }
+    }
+}
