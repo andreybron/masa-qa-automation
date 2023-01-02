f400d159574b4bb0df477cf2a9f70c6267b3936a
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-02 18:10:25 +0200

    Classwork 5

---
+++ Javaproject/src/lesson_5/RandomValues.java
@@ -0,0 +1,23 @@
+package lesson_5;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomValues {
+    public static void main(String[] args) {
+//  Random
+        Random random = new Random();
+        ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+
+        int randomInteger = random.nextInt();
+        int randomIntegerWithBound = random.nextInt(100);
+        double randomDouble = random.nextDouble();
+
+        System.out.println("randomInteger = " + randomInteger);
+        System.out.println("randomIntegerWithBound = " + randomIntegerWithBound);
+        System.out.println("randomDouble = " + randomDouble);
+
+        int randomIntegerWithBounds = threadLocalRandom.nextInt(10, 20);
+        System.out.println("randomIntegerWithBounds = " + randomIntegerWithBounds);
+    }
+}
