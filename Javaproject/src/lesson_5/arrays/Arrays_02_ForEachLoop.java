63632290816cc64f31c57abfe2282ab1a84cf91b
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-02 20:39:02 +0200

    lesson 5 update

--- Javaproject/src/lesson_5/arrays/Arrays_02_ForEachLoop.java
+++ Javaproject/src/lesson_5/arrays/Arrays_02_ForEachLoop.java
@@ -5,12 +5,15 @@ public class Arrays_02_ForEachLoop {
 
         int arrayOfNumbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    //itar
         for (int i = 0; i < arrayOfNumbers.length; i++) {
             System.out.print(arrayOfNumbers[i] + " ");
         }
         System.out.println();
 
-        for (int number : arrayOfNumbers) {
+        int array[] = new int[10];
+    //iter
+        for (int number : array) {
             System.out.print(number + " ");
         }
     }
