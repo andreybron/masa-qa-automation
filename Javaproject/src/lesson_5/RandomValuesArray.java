407e4435e5f0ae88f508e6a8a67facb7b547a1a3
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-02 18:58:20 +0200

    RandomValues

---
+++ Javaproject/src/lesson_5/RandomValuesArray.java
@@ -0,0 +1,24 @@
+package lesson_5;
+
+import java.util.Random;
+
+public class RandomValuesArray {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int array[] = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+            System.out.println("array[" + i + "] = " + array[i]);
+        }
+        System.out.println("***");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+        }
+ //       System.out.println();
+    }
+}
