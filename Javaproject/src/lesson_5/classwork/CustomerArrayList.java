7069e140db756b0b72f19e9c4ffd24c5f5a5579c
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-05 19:06:14 +0200

    Create CustomerArrayList.java

---
+++ Javaproject/src/lesson_5/classwork/CustomerArrayList.java
@@ -0,0 +1,14 @@
+package lesson_5.classwork;
+
+public class CustomerArrayList {
+    public static void main(String[] args) {
+
+    }
+}
+
+//    Используя Intellij IDEA, создайте класс CustomerArrayList.
+//        Создайте ArrayList на 10 чисел.
+//        Требуется:
+//        Вывести на экран: наибольшее значение массива, наименьшее значение массива, общую сумму всех элементов, среднее арифметическое всех элементов, вывести все нечетные значения.
+//        Используем forEach
+//        *Double.MIN_VALUE, Double.MAX_VALUE;
