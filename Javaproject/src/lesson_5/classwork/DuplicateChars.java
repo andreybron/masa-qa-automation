206cd3094d11a4021ba9a0193db21b50cfd52f8a
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-05 21:26:08 +0200

    Update DuplicateChars.java

--- Javaproject/src/lesson_5/classwork/DuplicateChars.java
+++ Javaproject/src/lesson_5/classwork/DuplicateChars.java
@@ -1,4 +1,34 @@
 package lesson_5.classwork;
 
+import java.util.HashMap;
+
 public class DuplicateChars {
+    public static void main(String[] args) {
+        String text= "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+        char [] textCharacters = text.toCharArray();
+        HashMap<Character, Integer> collection = new HashMap<>();
+
+        for (char letter : textCharacters) {
+
+            if (!collection.containsKey(letter)) {
+                collection.put(letter, 1);
+            }
+            else {
+                collection.put(letter, collection.get(letter) + 1);
+            }
+
+        }
+        collection.forEach((letter, number) -> System.out.print("'" + letter + "' " + number + "; "));
+        System.out.println();
+        System.out.println(collection.get("L"));
+
+
+    }
 }
+
+//    Используя IntelliJ IDEA, создайте класс DuplicateChars.
+//        Для заданной строки задача состоит в том, чтобы написать программу на Java, которая печатает количество вхождений каждого символа в строку.
+//        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+//        Aldus PageMaker including versions of Lorem Ipsum.
+//        Решить задачу подсчета повторяющихся элементов в массиве с помощью HashMap.
