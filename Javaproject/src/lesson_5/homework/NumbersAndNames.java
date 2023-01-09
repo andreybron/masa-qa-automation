a584375463038d356961a14cb6500558fb145e7b
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-09 16:17:25 +0200

    Update NumbersAndNames.java

--- Javaproject/src/lesson_5/homework/NumbersAndNames.java
+++ Javaproject/src/lesson_5/homework/NumbersAndNames.java
@@ -1,22 +1,44 @@
 package lesson_5.homework;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NumbersAndNames {
     public static void main(String[] args) {
 
+        System.out.println("---Numbers and names---");
         //1.create collection with 10 numbers
-        ArrayList<Integer>numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             numbers.add(i);
         }
 //        System.out.println("myArrayList.size() =" + numbers.size());
+//        numbers.forEach(number -> System.out.print(number + " "));
 
-        numbers.forEach(number -> System.out.print(number + " "));
-        
         //2.create collection with 10 names
+        ArrayList<String> names = new ArrayList<>();
 
+        names.add("Andrey");
+        names.add("Nikita");
+        names.add("Olga");
+        names.add("Marina");
+        names.add("Pavel");
+        names.add("Alex");
+        names.add("Egor");
+        names.add("Irina");
+        names.add("Karina");
+        names.add("Lisa");
 
+//        System.out.println("names.size() = " + names.size());
+//        System.out.println("names = " + names);
+
+        //3.create set with names and numbers
+        Set<String> finalCollection = new HashSet<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            finalCollection.add(numbers.get(i) + "-" + names.get(i));
+        }
+        finalCollection.forEach(name -> System.out.println(name));
     }
 }
 
