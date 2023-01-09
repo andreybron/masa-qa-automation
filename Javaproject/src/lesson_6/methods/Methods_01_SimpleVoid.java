fd74e5ed6234999fb5fb17451df69b2fb1b74b33
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-09 18:55:51 +0200

    Create Methods_01_SimpleVoid.java

---
+++ Javaproject/src/lesson_6/methods/Methods_01_SimpleVoid.java
@@ -0,0 +1,16 @@
+package lesson_6.methods;
+
+public class Methods_01_SimpleVoid {
+    public static void main(String[] args) {
+        sayHelloToClass();
+    }
+
+    public static void sayHelloToClass() {
+        System.out.println("Hello Alex!");
+        System.out.println("Hello Andrey!");
+        System.out.println("Hello Trump!");
+        System.out.println("Hello Obama!");
+        System.out.println("Hello Karina!");
+
+    }
+}
