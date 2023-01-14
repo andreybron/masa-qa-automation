3cc271a9bd50c3e74ad470f36eb9cc3f9270ad4c
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-15 01:53:24 +0200

    Create Car.java

---
+++ Javaproject/src/lesson_7/car/Car.java
@@ -0,0 +1,18 @@
+package lesson_7.car;
+
+public class Car {
+
+    String manufacturer;
+    String model;
+    int year;
+    String color;
+    boolean isElectrical;
+
+    public Car() {
+
+    }
+    public void carIsReady() {
+        System.out.println("I'm ready!");
+    }
+
+}
