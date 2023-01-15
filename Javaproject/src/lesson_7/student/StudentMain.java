4fd7ddbdb211c49c384126e40ba0e003a68b58a7
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-15 04:46:23 +0200

    Update StudentMain.java

--- Javaproject/src/lesson_7/student/StudentMain.java
+++ Javaproject/src/lesson_7/student/StudentMain.java
@@ -1,5 +1,10 @@
 package lesson_7.student;
 
 public class StudentMain {
+    public static void main(String[] args) {
 
+        Student andrey = new Student("Andrey", -33);
+
+        System.out.println(andrey);
+    }
 }
