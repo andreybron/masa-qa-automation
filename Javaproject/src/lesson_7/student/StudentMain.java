96be5a0688f33d8282d4c781a072c720353ade9c
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-15 05:08:17 +0200

    Update StudentMain.java

--- Javaproject/src/lesson_7/student/StudentMain.java
+++ Javaproject/src/lesson_7/student/StudentMain.java
@@ -3,8 +3,12 @@ package lesson_7.student;
 public class StudentMain {
     public static void main(String[] args) {
 
-        Student andrey = new Student("Andrey", -33);
+        Student student = new Student("Andrey", 33);
+        System.out.println("student.getName() = " + student.getName());
+        System.out.println("student.getAge() = " + student.getAge());
 
-        System.out.println(andrey);
+        student.setAge(-3);
+        student.setName("Bro");
+        System.out.println(student);
     }
 }
