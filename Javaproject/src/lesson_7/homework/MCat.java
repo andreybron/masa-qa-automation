f0a61532f55de45611131b0c38ff7010b32197c9
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-15 05:31:21 +0200

    Update MCat.java

--- Javaproject/src/lesson_7/homework/MCat.java
+++ Javaproject/src/lesson_7/homework/MCat.java
@@ -2,6 +2,14 @@ package lesson_7.homework;
 
 public class MCat {
     public static void main(String[] args) {
-        
+
+        Cat firstCat = new Cat("mr.Black", "black", 7);
+        System.out.println("firstCat = " + firstCat);
+        firstCat.setAge(-1);
+        System.out.println("firstCat.getAge() = " + firstCat.getAge());
     }
 }
+
+//Создайте класс MCats и создайте метод main.
+//Создайте список из 5 котов.
+//Создайте цикл который пройдется по списку и напечатает информацию о каждом котенке.
