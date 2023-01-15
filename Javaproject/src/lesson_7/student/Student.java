9f732b1d64e537ad29ec0e78df24a11bc8bc3485
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-15 04:46:26 +0200

    Update Student.java

--- Javaproject/src/lesson_7/student/Student.java
+++ Javaproject/src/lesson_7/student/Student.java
@@ -1,4 +1,29 @@
 package lesson_7.student;
 
+import java.util.Scanner;
+
 public class Student {
+
+    String name;
+    int age;
+    public Scanner input;
+
+    public Student(String name, int age) {
+        while (age < 18 || age > 150) {
+            System.out.println("Error! For student " + name + " age out of range! Input manual:");
+            input = new Scanner(System.in);
+            age = input.nextInt();
+        }
+        input.close();
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
