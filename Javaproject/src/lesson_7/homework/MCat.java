a8992cd6a1a033f27609f033ee090f3440caddfa
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-15 06:13:36 +0200

    Update MCat.java

--- Javaproject/src/lesson_7/homework/MCat.java
+++ Javaproject/src/lesson_7/homework/MCat.java
@@ -1,12 +1,17 @@
 package lesson_7.homework;
 
+import java.util.ArrayList;
 public class MCat {
     public static void main(String[] args) {
 
-        Cat firstCat = new Cat("mr.Black", "black", 7);
-        System.out.println("firstCat = " + firstCat);
-        firstCat.setAge(-1);
-        System.out.println("firstCat.getAge() = " + firstCat.getAge());
+        ArrayList<Cat> catsList = new ArrayList<>();
+        catsList.add(new Cat("mr.Black", "black", 4));
+        catsList.add(new Cat("mrs.White", "white", 5));
+        catsList.add(new Cat("FOX", "orange", 3));
+        catsList.add(new Cat("Brita", "grey", 1));
+        catsList.add(new Cat("Submarine", "yellow", -5));
+
+        catsList.forEach(cat -> System.out.println(cat));
     }
 }
 
