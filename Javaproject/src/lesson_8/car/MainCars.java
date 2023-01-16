0d6fcf57796e20b8317c4c59d01d125fe0619102
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-16 19:14:06 +0200

    Car classwork

--- Javaproject/src/lesson_8/car/MainCars.java
+++ Javaproject/src/lesson_8/car/MainCars.java
@@ -1,4 +1,20 @@
 package lesson_8.car;
 
 public class MainCars {
+    public static void main(String[] args) {
+
+        Engine engine = new Engine(250, "v6", false);
+
+        Wheel[] wheels = new Wheel[5];
+        for (int i = 0; i < wheels.length; i++) {
+            wheels[i] = new Wheel(19, 2.5);
+        }
+        Car car = new Car("BMW", "m3", 2023, engine, wheels);
+
+        System.out.println("car = " + car);
+
+        car.wheels[1].radius = -5;
+
+        System.out.println("car = " + car);
+    }
 }
