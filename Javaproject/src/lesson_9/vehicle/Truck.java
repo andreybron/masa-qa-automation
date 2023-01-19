12a734adc36856656bfc7c5bbc20bdfb9d6e19dd
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 21:48:43 +0200

    Create Truck.java

---
+++ Javaproject/src/lesson_9/vehicle/Truck.java
@@ -0,0 +1,38 @@
+package lesson_9.vehicle;
+
+public class Truck extends Vehicle{
+    private String type;
+    private String color;
+
+    public Truck(String manufacturer, int year, String type, String color) {
+        super(manufacturer, year);
+        this.type = type;
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void sayHello() {
+        super.sayHello();
+    }
+
+    @Override
+    public void whoAreYou() {
+
+    }
+}
