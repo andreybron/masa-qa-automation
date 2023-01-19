fb41bf5910efc4333ee556815a893fea734b191b
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 21:48:41 +0200

    Create Bus.java

---
+++ Javaproject/src/lesson_9/vehicle/Bus.java
@@ -0,0 +1,33 @@
+package lesson_9.vehicle;
+
+public class Bus extends Vehicle{
+    private long capacity;
+    private String color;
+
+    public Bus(String manufacturer, int year, long capacity, String color) {
+        super(manufacturer, year);
+        this.capacity = capacity;
+        this.color = color;
+    }
+
+    public long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(long capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void whoAreYou() {
+
+    }
+}
