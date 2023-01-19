3455789283099da8a4107fd9e57dbdf831b785ea
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 18:44:00 +0200

    Create Person.java

---
+++ Javaproject/src/lesson_9/Person.java
@@ -0,0 +1,46 @@
+package lesson_9;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean isJewish;
+
+    public Person(String firstName, String lastName, int age, boolean isJewish) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.isJewish = isJewish;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isJewish() {
+        return isJewish;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", isJewish=" + isJewish +
+                '}';
+    }
+
+    public void whatIsYourName() {
+        System.out.println("My name is " + firstName + lastName + "");
+    }
+}
