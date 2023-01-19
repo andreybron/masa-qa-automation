39a9038a002b8d09e39effe8903e6c4f02fbab65
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 21:48:49 +0200

    Create Car.java

---
+++ Javaproject/src/lesson_9/vehicle/Car.java
@@ -0,0 +1,38 @@
+package lesson_9.vehicle;
+
+public class Car extends Vehicle{
+    private boolean isElectrical;
+    private String color;
+
+    public Car(String manufacturer, int year, boolean isElectrical, String color) {
+        super(manufacturer, year);
+        this.isElectrical = isElectrical;
+        this.color = color;
+    }
+
+    public boolean isElectrical() {
+        return isElectrical;
+    }
+
+    public void setElectrical(boolean electrical) {
+        isElectrical = electrical;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void sayHello() {
+        super.sayHello();
+    }
+
+    @Override
+    public void whoAreYou() {
+
+    }
+}
