0092e56785258ca6e4e6c1eed4a9cdffef9aa606
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 20:54:47 +0200

    Create Queen.java

---
+++ Javaproject/src/lesson_9/checkmate/Queen.java
@@ -0,0 +1,18 @@
+package lesson_9.checkmate;
+
+public class Queen extends Figure{
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public boolean canYouMove() {
+        return false;
+    }
+
+    @Override
+    public void introduceYourSelf() {
+
+    }
+}
