8604abacacf48929ed87ffcc135cb8eb6e8889c4
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 20:54:50 +0200

    Create Rook.java

---
+++ Javaproject/src/lesson_9/checkmate/Rook.java
@@ -0,0 +1,19 @@
+package lesson_9.checkmate;
+
+public class Rook extends Figure{
+
+    @Override
+    public void move() {
+        
+    }
+
+    @Override
+    public boolean canYouMove() {
+        return false;
+    }
+
+    @Override
+    public void introduceYourSelf() {
+
+    }
+}
