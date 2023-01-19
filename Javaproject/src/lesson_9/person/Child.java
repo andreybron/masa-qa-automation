0ddc61143f5182810f6a572704f867bd48947ab5
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-19 21:12:06 +0200

    Create Child.java

---
+++ Javaproject/src/lesson_9/person/Child.java
@@ -0,0 +1,12 @@
+package lesson_9.person;
+
+public class Child extends Person {
+    public Child(String firstName, String lastName, int age, boolean isJewish) {
+        super(firstName, lastName, age, isJewish);
+    }
+
+    @Override
+    public void whoAreYou() {
+        System.out.println("I");
+    }
+}
