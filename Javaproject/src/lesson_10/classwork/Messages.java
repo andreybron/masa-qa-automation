191e80e3a88098409ac09cc37c44854515b4b651
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-22 18:16:40 +0200

    Messages class created

--- Javaproject/src/lesson_10/classwork/Messages.java
+++ Javaproject/src/lesson_10/classwork/Messages.java
@@ -1,4 +1,30 @@
 package lesson_10.classwork;
 
 public class Messages {
+
+    public static void printMainMenu() {
+        System.out.println("Please choose");
+        System.out.println("1. Add new shape");
+        System.out.println("2. List all shapes");
+        System.out.println("3. Sum all circumferences");
+        System.out.println("4. Sum all areas");
+        System.out.println("5. Find biggest circumference");
+        System.out.println("6. Find biggest area");
+        System.out.println("7. Exit");
+    }
+
+    public static void printShapesMenu() {
+        System.out.println("Please choose from the next Shapes:");
+        System.out.println("1. Square");
+        System.out.println("2. Rectangle");
+        System.out.println("3. Circle");
+        System.out.println("4. Right triangle");
+    }
 }
+//Add new shape
+//List all shapes
+//Sum all circumferences
+//Sum all areas
+//Find biggest circumference
+//Find biggest area
+//Exit
