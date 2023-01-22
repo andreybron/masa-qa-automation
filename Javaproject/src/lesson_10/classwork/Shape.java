fb4f347b152943e864e2abbee7c37af650acb5bb
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-22 17:36:24 +0200

    Update Shape.java

--- Javaproject/src/lesson_10/classwork/Shape.java
+++ Javaproject/src/lesson_10/classwork/Shape.java
@@ -1,6 +1,18 @@
 package lesson_10.classwork;
 
-public class Shape {
+public abstract class Shape {
 
-    
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract double calculateArea();
+
+    public abstract double calculatePerimeter();
 }
