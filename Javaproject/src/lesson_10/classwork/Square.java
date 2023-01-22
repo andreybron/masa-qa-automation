1083df96af10f89d3b1c6ca69b021c5c3afd5bff
Andrey <46926928+andreybron@users.noreply.github.com>
2023-01-22 17:58:04 +0200

    Update Square.java

--- Javaproject/src/lesson_10/classwork/Square.java
+++ Javaproject/src/lesson_10/classwork/Square.java
@@ -1,4 +1,28 @@
 package lesson_10.classwork;
 
-public class Square {
+public class Square extends Shape {
+
+    private double side;
+
+    public Square(String color, double side) {
+        super(color);
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                " " + super.toString();
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return side * 4;
+    }
 }
