36122e91d662c557ba16436d4db7bd5928258f8e
Andrey <46926928+andreybron@users.noreply.github.com>
2023-02-02 19:17:35 +0200

    Update ControlledTryCatch.java

--- Javaproject/src/lesson_13/exceptions/ControlledTryCatch.java
+++ Javaproject/src/lesson_13/exceptions/ControlledTryCatch.java
@@ -3,16 +3,20 @@ package lesson_13.exceptions;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Date;
 
 public class ControlledTryCatch {
 
     public static void main(String[] args) {
 
         File ourFile = new File("D://file.txt");
+        Date date = new Date();
+
         try {
             FileReader reader = new FileReader(ourFile);
         } catch (FileNotFoundException e) {
-            System.out.println("WTF?");
+            System.out.println("WTF? " + date.getTime());
+//            System.out.println(exception.getMessage());
         }
     }
 }
